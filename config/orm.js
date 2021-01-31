cc5953cd46d71c3be7c6a266414266c78b7dc72e
Maria Ortega <mariaortegapb@gmail.com>
2021-01-31 03:51:23 -0500

    added comments to files

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,43 @@
 //sql connection imported
 var connection = require("../config/connection.js");
 
+//this function helps with the sql syntax which allows us to pass 3 values
+//into the sql query. To write query, we add 3 ?s. This function creates an 
+//array of ?s
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
 
 
-  
-//   // Object for SQL statement functions.
+// this helper function converts object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  //pushes key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    //skips hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+     //if string w/ spaces = '' (Michael Jackson => 'Michael Jackson')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+  //ex: {name: 'Michael Jackson'} => ["name= 'Michael Jackson'"]
+      arr.push(key + "=" + value);
+    }
+  }
+  //makes array of strings = single comma-separated string
+  return arr.toString();
+}
+
+
+// Object for SQL functions
   var orm = {
     all: function(tableInput, cb) {
       var queryString = "SELECT * FROM " + tableInput + ";";
@@ -54,15 +87,6 @@ var connection = require("../config/connection.js");
       });
     },
    
-
-
-
-
-
-
-
-
-
 
 // Export orm object for the model 
 module.exports = orm;
