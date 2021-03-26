113676d810f64bfe931cef662a4e63ad50324893
Maria Ortega <mariaortegapb@gmail.com>
2021-03-26 12:41:09 -0400

    working on handlebars

--- models/burger.js
+++ models/burger.js
@@ -2,23 +2,28 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function(cb) {
-      orm.all("burgers", function(res) {
-        cb(res);
-      });
-    },
-    // variables cols and vals = arrays
-    create: function(cols, vals, cb) {
-      orm.create("burgers", cols, vals, function(res) {
-        cb(res);
-      });
-    },
-    update: function(objColVals, condition, cb) {
-      orm.update("burgers", objColVals, condition, function(res) {
-        cb(res);
-      });
-      }
-    };
+  all: function (cb) {
+    orm.all("burgers", function (res) {
+      cb(res);
+    });
+  },
+  // variables cols and vals = arrays
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  update: function (objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
+  delete: function (condition, cb) {
+    orm.delete("burgers", condition, function (res) {
+      cb(res);
+    });
+  },
+};
 
-  // Export the database functions for the controller (burgers_controller.js).
+// Export the database functions for the controller (burgers_controller.js).
 module.exports = burger;
