161411499550969e4a99461727cb00b8d832db92
Maria Ortega <mariaortegapb@gmail.com>
2021-04-10 16:52:31 -0400

    setting up heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 // MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Jazz1*di",
-  database: "burgers_db"
-});
+var connection = mysql.createConnection(
+  process.env.JAWSDB_URL || {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Jazz1*di",
+    database: "burgers_db",
+  }
+);
 
 //to connect
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
